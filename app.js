c9198273fab875311d1ddc8314f8f39e56e7cb61
ferdavcop <davidcopte@gmail.com>
2023-11-07 16:10:53 -0300

    corrigiendo errores

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+const port = process.env.PORT || 3000
 
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath) );
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000", 3000);
+app.listen( port, () => {
+    console.log("Server is running on port 3000");
 });
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
 
-app.get('/', (req, res) => {
+app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'));
 });
 
-app.get('/', (req, res) => {
+app.get('/login', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/login.html'));
 })
