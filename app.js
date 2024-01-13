6d66d7bf14321d56de79f81c4074357ee95eaa5b
ferdavcop <davidcopte@gmail.com>
2024-01-13 09:09:16 -0300

    agregando arreglos

--- app.js
+++ app.js
@@ -3,21 +3,21 @@ const path = require('node:path');
 const app = express();
 const port = process.env.PORT || 3000
 
-const publicPath = path.resolve(__dirname, './public');
+const publicPath = path.join(__dirname, './public');
 app.use(express.static(publicPath) );
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+})
 app.listen( port, () => {
     console.log("Server is running on port 3000");
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
-});
-
-app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/register.html'));
-});
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/login.html'));
-})
