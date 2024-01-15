5b0b59f7797a15bd7baa8ea876434f63e379e94f
ferdavcop <davidcopte@gmail.com>
2024-01-15 19:49:58 -0300

    haciendo arreglos

--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const path = require('node:path');
 const app = express();
+const path = require('node:path');
 const port = process.env.PORT || 3000
 
 const publicPath = path.join(__dirname, './public');
 app.use(express.static(publicPath) );
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/home.html'));
+    res.sendFile(path.join(__dirname, 'views','home.html'));
 });
 
 app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/register.html'));
+    res.sendFile(path.join(__dirname, 'views','register.html'));
 });
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/login.html'));
+    res.sendFile(path.join(__dirname, 'views','login.html'));
 })
 app.listen( port, () => {
     console.log("Server is running on port 3000");
